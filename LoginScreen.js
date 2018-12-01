260b883ecde5167d6de2e4125c5f845255297c0b
christopherjose <cjose@luc.edu>
2018-11-30 23:44:47 -0600

    saving

--- LoginScreen.js
+++ LoginScreen.js
@@ -1,51 +1,31 @@
 import React, {Component} from "react";
-//import {Text, View, StyleSheet} from 'react-native';
-import {Text, View, StyleSheet ,Container, Content, Header, Form, Input, Button, Item, Label} from 'react-native';
-// will implement firebase
+import {Text, View, StyleSheet} from 'react-native';
+//import {Text, View, StyleSheet ,Container, Content, Header, Form, Input, Button, Item, Label} from 'react-native';
+//will implement firebase
 
 export default class LoginScreen extends React.Component {
     render() {
         return (
-            <Container style ={styles.container}>
-                <Form>
-                    <Item>
-                        <Label>Email</Label>
-                            <Input
-                                autoCorrect = {false}
-                                autoCapitalization = "none"
-                            />
-                    </Item>
+            <View styles={styles.container}>
+                <Button
+                    title = "Email"
+                /> 
 
-                    <Item>
-                        <Label>Password</Label>
-                            <Input
-                                secureTextEntry = {true}
-                                autoCorrect = {false}
-                                autoCapitalization = "none"
-                            />
-                    </Item>
-
-                    <Button style={{marginTop: 10}}>
-                        full
-                        rounded
-                        success 
-                    </Button>
-                        <Text>Log In</Text>
-                </Form>
-            </Container>
-            
+                <Button 
+                    title = "Password"
+                />
+            </View> 
         );
     }
 }
 
 const styles = StyleSheet.create(
     {
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-        //alignItems: 'center',
-        justifyContent: 'center',
-        padding: 10,
+        container: {
+            flex: 1,
+            backgroundColor: 'cyan',
+            alignItems: 'center',
+            justifyContent: 'center'
+        }   
     }
-}
 );
